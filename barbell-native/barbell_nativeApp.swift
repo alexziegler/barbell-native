2e491708e754fad8218733d7b4a34d80a047f366
Alex Ziegler <8752635+alexziegler@users.noreply.github.com>
2026-01-17 12:04:23 +0100

    Initial Commit

---
+++ barbell-native/barbell_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  barbell_nativeApp.swift
+//  barbell-native
+//
+//  Created by Alex Ziegler on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct barbell_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
